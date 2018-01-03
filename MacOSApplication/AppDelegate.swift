a9ec82cdc0e2039b8ca59b364ae157727dcd17e1
QuantumSpark <jamespark1234@hotmail.com>
2018-01-02 20:25:44 -0800

    Initial Commit

---
+++ MacOSApplication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOSApplication
+//
+//  Created by James Park on 2018-01-02.
+//  Copyright © 2018 James Park. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOSApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOSApplication
+//
+//  Created by James Park on 2018-01-02.
+//  Copyright © 2018 James Park. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
