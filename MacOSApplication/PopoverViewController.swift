f0cad8d28e1eee4ed91b059a29be0190a78b0bf8
Johnson Liu <liujohnson118@yahoo.com>
2018-02-04 14:41:44 -0800

    adding logic in popover window

--- MacOSApplication/PopoverViewController.swift
+++ MacOSApplication/PopoverViewController.swift
@@ -10,10 +10,44 @@ import Cocoa
 
 class PopoverViewController: NSViewController {
 
+    @IBAction func zoomIn(_ sender: Any) {
+        
+    }
+    
+
+    @IBOutlet weak var cameraExposure: NSSlider!
+    
+    @IBAction func cameraExposure(_ sender: Any) {
+    }
+    @IBAction func zoomOut(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var redBalance: NSSlider!
+    
+    @IBOutlet weak var greenBalance: NSSlider!
+    
+    @IBOutlet weak var blueBalance: NSSlider!
+    
+    @IBAction func changeRedBalance(_ sender: Any) {
+    }
+    
+    @IBAction func changeGreenBalance(_ sender: Any) {
+    }
+    
+    @IBAction func changeBlueBalance(_ sender: Any) {
+    }
+    
     var test = "Hello"
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello")
+        cameraExposure.maxValue = 1;
+        cameraExposure.minValue = 0;
+        redBalance.maxValue = 4;
+        redBalance.minValue = 1;
+        greenBalance.maxValue = 4;
+        greenBalance.minValue = 1;
+        blueBalance.maxValue = 4;
+        blueBalance.minValue = 1;
     }
     
 }
---
+++ PopoverViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PopoverViewController.swift
+//  MacOSApplication
+//
+//  Created by Geyi Liu on 2018-02-04.
+//  Copyright © 2018 James Park. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func click(_ sender: Any) {
+        print("FUCK")
+    }
+}
