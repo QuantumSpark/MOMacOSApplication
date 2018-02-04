22256dd0be33ab3d30dfd45cfa7d38b9ae6b69d6
Johnson Liu <liujohnson118@yahoo.com>
2018-02-04 15:11:16 -0800

    removed buttons from collection view

--- MacOSApplication/CollectionViewItem.swift
+++ MacOSApplication/CollectionViewItem.swift
@@ -41,27 +41,11 @@ class CollectionViewItem: NSCollectionViewItem, NSPopoverDelegate {
     @IBOutlet weak var playButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
     @IBOutlet weak var downloadButton: NSButton!
-//    @IBOutlet weak var cameraExposure: NSSlider!
-//    @IBOutlet weak var whiteBalanceRed: NSSlider!
-//    @IBOutlet weak var whiteBalanceGreen: NSSlider!
-//    @IBOutlet weak var whiteBalanceBlue: NSSlider!
-    
-//    @IBAction func changeExposure(_ sender: Any) {
-//        let packet = Packet(type: .changeExposure, id: id, payload: NSData(bytes: &cameraExposure.floatValue, length: MemoryLayout<Float>.size) as Data)
-//        dataIPad.socket.write(packet.serialize(), withTimeout: -1, tag: id)
-//    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addDisplayLayer()
-//        cameraExposure.maxValue = 1;
-//        cameraExposure.minValue = 0;
-//        whiteBalanceRed.maxValue = 4;
-//        whiteBalanceGreen.maxValue = 4;
-//        whiteBalanceBlue.maxValue = 4;
-//        whiteBalanceRed.minValue = 1;
-//        whiteBalanceGreen.minValue = 1;
-//        whiteBalanceBlue.minValue = 1;
 
         popUpViewController = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil).instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "PopoverViewController")) as! PopoverViewController
 //        [self.storyboard instantiateControllerWithIdentifier:@"PopoverViewController"];
@@ -89,15 +73,6 @@ class CollectionViewItem: NSCollectionViewItem, NSPopoverDelegate {
         stopStreaming()
     }
 
-//    @IBAction func zoomIn(_ sender: Any) {
-//        let packet = Packet(type: .zoomIn, id: id)
-//        dataIPad.socket.write(packet.serialize(), withTimeout: -1, tag: id)
-//    }
-//    
-//    @IBAction func zoomOut(_ sender: Any) {
-//        let packet = Packet(type: .zoomOut, id: id)
-//        dataIPad.socket.write(packet.serialize(), withTimeout: -1, tag: id)
-//    }
 
     @IBAction func displayPopover(_ sender: Any) {
 
@@ -151,21 +126,6 @@ class CollectionViewItem: NSCollectionViewItem, NSPopoverDelegate {
 
     }
     
- 
-//    @IBAction func changeWhiteBalanceRed(_ sender: Any) {
-//        let packet = Packet(type: .redGain, id: id, payload: NSData(bytes: &whiteBalanceRed.floatValue, length: MemoryLayout<Float>.size) as Data)
-//        dataIPad.socket.write(packet.serialize(), withTimeout: -1, tag: id)
-//    }
-//
-//    @IBAction func changeWhiteBalanceGreen(_ sender: Any) {
-//        let packet = Packet(type: .greenGain, id: id, payload: NSData(bytes: &whiteBalanceGreen.floatValue, length: MemoryLayout<Float>.size) as Data)
-//        dataIPad.socket.write(packet.serialize(), withTimeout: -1, tag: id)
-//    }
-//
-//    @IBAction func changeWhiteBalanceBlue(_ sender: Any) {
-//        let packet = Packet(type: .blueGain, id: id, payload: NSData(bytes: &whiteBalanceBlue.floatValue, length: MemoryLayout<Float>.size) as Data)
-//        dataIPad.socket.write(packet.serialize(), withTimeout: -1, tag: id)
-//    }
 
     override func viewDidAppear() {
         startStreaming()
